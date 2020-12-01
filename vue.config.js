a2703b3b12d646951108d473c2dddac39a780441
ramigs <ramigs@gmail.com>
2020-12-01 11:56:15 +0000

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack')
+
+module.exports = {
+    filenameHashing: false,
+    configureWebpack: {
+        plugins: [
+            new webpack.optimize.LimitChunkCountPlugin({
+                maxChunks: 1
+            })
+        ]
+    },
+    chainWebpack:
+        config => {
+            config.optimization.delete('splitChunks')
+        }
+}
